64392b829ec899df1e1900b2bf108e36161025d8
oxanahr <oxana.hristova@gmail.com>
2018-05-06 15:07:01 +0300

    Update MechanicalTreatedDish.java

--- CookingEBook/src/com/telerikacademy/cooking/MechanicalTreatedDish.java
+++ CookingEBook/src/com/telerikacademy/cooking/MechanicalTreatedDish.java
@@ -1,31 +1,39 @@
 package com.telerikacademy.cooking;
 
 import com.telerikacademy.enumerations.DishCategory;
-import com.telerikacademy.enumerations.MechanicalTreatType;
 import com.telerikacademy.exceptions.NoSuchTreatmentException;
 
 import java.sql.Timestamp;
 
 public class MechanicalTreatedDish extends Dish {
 
+    public enum MechanicalTreatType {
+        CUTTING,
+        EXTRACTING,
+        BLENDING,
+        MIXING,
+        MARINATING
+    }
+
     private MechanicalTreatType type;
 
     public MechanicalTreatedDish(DishCategory category, Recipe recipe, MechanicalTreatType type) {
-        super( category, recipe);
+        super( category, recipe );
         super.getTime();
-        this.setType(type);
+        this.setType( type );
     }
 
     public MechanicalTreatType getType() {
         return type;
     }
 
-    public void setType(MechanicalTreatType type) {
-        for (MechanicalTreatType u: MechanicalTreatType.values())
-        {
-            if (!type.equals(u)) {
-                throw new NoSuchTreatmentException( type.toString() );
-            }
+    private void setType(MechanicalTreatType type) {
+        if (!(type.equals( MechanicalTreatType.BLENDING ) ||
+                type.equals( MechanicalTreatType.CUTTING ) ||
+                type.equals( MechanicalTreatType.EXTRACTING ) ||
+                type.equals( MechanicalTreatType.MIXING ) ||
+                type.equals( MechanicalTreatType.MARINATING ))) {
+            throw new NoSuchTreatmentException( type.toString() );
         }
         this.type = type;
     }
