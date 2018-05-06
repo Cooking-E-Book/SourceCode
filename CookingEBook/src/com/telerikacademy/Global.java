9f258c33b04b9a7bf11a8485028836bbaddacb92
oxanahr <oxana.hristova@gmail.com>
2018-05-06 15:35:15 +0300

    Add files via upload

---
+++ CookingEBook/src/com/telerikacademy/Global.java
@@ -0,0 +1,17 @@
+package com.telerikacademy;
+
+import com.telerikacademy.users.User;
+import com.telerikacademy.users.Visitor;
+
+public class Global {
+
+    // Global variables
+
+    public static User currentUser = new Visitor("visitor");
+    public static int RECIPE_ID = 1;
+    public static int STEP_ID = 1;
+    public static int UTENSIL_ID = 1;
+    public static int INGREDIENT_ID = 1;
+
+
+}
