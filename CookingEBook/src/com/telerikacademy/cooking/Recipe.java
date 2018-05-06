d94ea2d49ff6a8971a4591596b9bde0be37dfd2c
oxanahr <oxana.hristova@gmail.com>
2018-05-06 15:08:37 +0300

    Update Recipe.java

--- CookingEBook/src/com/telerikacademy/cooking/Recipe.java
+++ CookingEBook/src/com/telerikacademy/cooking/Recipe.java
@@ -1,7 +1,8 @@
 package com.telerikacademy.cooking;
 
-import com.telerikacademy.Globals;
-import com.telerikacademy.exceptions.incompatibility.IncompatibleUnitTypes;
+import com.telerikacademy.Global;
+import com.telerikacademy.interfaces.Component;
+import com.telerikacademy.messages.Comment;
 import com.telerikacademy.users.User;
 
 import java.awt.*;
@@ -10,106 +11,86 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
-/*
-Recipe                                         string?
-    ingredient List HashMap<Ingredient, quantity> ingedient
-    Description
-    User - creator
-    List User - People who tried it
-    Rating
-    Date & time recipe added
-    +List Category categories
-    +Preparation time
-    +Cooking Time
-    +Ready time = preparation + cooking
-    +Image images
- */
+
 public class Recipe {
     private int id;
     private String title;
     private User author;
-    private Map<Double, Component> recipe = new HashMap<>();
     private String description;
     private Image image;
     private Queue<Step> steps;
+    private Map<String, Component> recipe = new HashMap<>();
+    private List<Comment> comments;
     private Timestamp recipeAdded;
-    
-    public int getId() {
-        return id;
-    }
-    
-    public void setId(int id) {
-        this.id = id;
-    }
-    
-    public String getTitle() {
-        return title;
-    }
-    
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    
-    public User getAuthor() {
-        return author;
-    }
-    
-    public void setAuthor(User author) {
-        this.author = author;
-    }
-    
-    public Map<Double, Component> getRecipe() {
-        return recipe;
-    }
-    
-    public void setRecipe(Map<Double, Component> recipe) {
-        this.recipe = recipe;
-    }
-    
-    public String getDescription() {
-        return description;
-    }
-    
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    
-    public Image getImage() {
-        return image;
-    }
-    
-    public void setImage(Image image) {
-        this.image = image;
-    }
-    
-    public Queue<Step> getSteps() {
-        return steps;
-    }
-    
-    public void setSteps(Queue<Step> steps) {
-        this.steps = steps;
-    }
-    
-    public Timestamp getRecipeAdded() {
-        return recipeAdded;
-    }
-    
-    public void setRecipeAdded(Timestamp recipeAdded) {
-        this.recipeAdded = recipeAdded;
-    }
-    
+
     public Recipe(String title, User author, String description) {
-         this.id = Globals.recipeId++;
+        this.setId();
         this.title = title;
         this.author = author;
         this.description = description;
         steps = new LinkedList<>();
         recipe = new HashMap<>();
+        comments = new ArrayList<>();
+        this.recipeAdded = new Timestamp(System.currentTimeMillis());
     }
-    
-    
-    protected  void addStep(Step step){}
 
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId() {
+        this.id = Global.RECIPE_ID++;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public Map<String, Component> getRecipe() {
+        return recipe;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public Queue<Step> getSteps() {
+        return steps;
+    }
+
+    public Timestamp getRecipeAdded() {
+        return recipeAdded;
+    }
+
+    public void addIngredient(String amount, Component ingredient) {
+        this.recipe.put( amount, ingredient );
+    }
+
+
+    public void addStep(Step step) {
+        this.steps.add( step );
+    }
+
+    public void addComment(Comment comment) {
+        this.comments.add( comment );
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format( "Recipe %s. %s, Author -> %s%n%s ", this.getId(), this.getTitle(), this.getAuthor(),
+                this.getDescription() );
+    }
 }
