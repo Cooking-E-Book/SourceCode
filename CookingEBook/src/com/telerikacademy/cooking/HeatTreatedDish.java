f147424da8f571fa047a05b215c587eede05547e
oxanahr <oxana.hristova@gmail.com>
2018-05-06 15:05:33 +0300

    Update HeatTreatedDish.java

--- CookingEBook/src/com/telerikacademy/cooking/HeatTreatedDish.java
+++ CookingEBook/src/com/telerikacademy/cooking/HeatTreatedDish.java
@@ -1,7 +1,9 @@
 package com.telerikacademy.cooking;
 
 import com.telerikacademy.enumerations.DishCategory;
+import com.telerikacademy.enumerations.Unit;
 import com.telerikacademy.exceptions.NoSuchTreatmentException;
+import com.telerikacademy.exceptions.measurement.NoSuchBulkMeasurementException;
 
 import java.sql.Timestamp;
 
@@ -24,7 +26,7 @@ public class HeatTreatedDish extends Dish {
         super(category, recipe);
         super.getTime();
         this.temperature = temperature;
-        this.heatTreatType = heatTreatType; //this.setType(heatTreatType);
+        this.setType(heatTreatType);
     }
 
     public Double getTemperature() {
@@ -39,15 +41,18 @@ public class HeatTreatedDish extends Dish {
         return heatTreatType;
     }
 
-    /*public void setType(HeatTreatType heatTreatType) {
-        for (HeatTreatType u: HeatTreatType.values())
-        {
-            if (!heatTreatType.equals(u)) {
-                throw new NoSuchTreatmentException( heatTreatType.toString() );
-            }
+    private void setType(HeatTreatType heatTreatType) {
+        if (!(heatTreatType.equals( HeatTreatType.BAKING)
+                || heatTreatType.equals(HeatTreatType.BOILING)
+                || heatTreatType.equals(HeatTreatType.FRYING)
+                || heatTreatType.equals(HeatTreatType.OPENFLAME)
+                || heatTreatType.equals(HeatTreatType.MICROWAVED)
+                || heatTreatType.equals(HeatTreatType.GRILL)
+                || heatTreatType.equals(HeatTreatType.STEAMED))) {
+            throw new NoSuchTreatmentException( heatTreatType.toString() );
         }
 
         this.heatTreatType = heatTreatType;
-    }*/
+    }
 
 }
